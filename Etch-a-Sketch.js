14756af626f57ffb764814b1fa3834454a417983
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-05 16:53:07 +0000

    made a grid using CSS

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 16; i++){
+    for (let i = 1; i <= 16; i++){
+        const div = document.createElement("div");
+        div.className = `box ${i}`
+        document.querySelector(".boxCont").appendChild(div);
+    }
+
+    let div = document.createElement("div");
+        div.className = "box17"
+        document.querySelector(".boxCont").appendChild(div);
+}
