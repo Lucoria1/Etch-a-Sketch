2f39800d4194e8f5a9298f1acafc09e165ea4187
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-06 12:22:13 +0000

    added create grid functionality

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,5 +1,8 @@
-for (let i = 1; i <= 16; i++){
-    for (let i = 1; i <= 16; i++){
+createGrid(16)
+
+function createGrid (param) {
+    for (let i = 1; i <= param; i++){
+    for (let i = 1; i <= param; i++){
         const div = document.createElement("div");
         div.className = `box`
         document.querySelector(".boxCont").appendChild(div);
@@ -8,7 +11,7 @@ for (let i = 1; i <= 16; i++){
     let div = document.createElement("div");
         div.className = "break"
         document.querySelector(".boxCont").appendChild(div);
-}
+}}
 
 document.querySelectorAll(".box").forEach((item) => {
     item.addEventListener("mouseover", (event) => {
@@ -17,7 +20,16 @@ document.querySelectorAll(".box").forEach((item) => {
 });
 
 document.querySelector(".newGrid").addEventListener("click", () => {
+    let boxNum = prompt("How wide do you want your grid?")
+
     document.querySelectorAll(".box").forEach((item) => {
         item.remove();
     })
+
+    if(boxNum <= 100){
+    createGrid(boxNum);} else if(boxNum === null) {
+        createGrid(16)} else if(boxNum > 100){
+            alert("Grid too large! Pick a number between 1-100!")
+            } else {
+                alert("Pick a number between 1-100!")}
 })
