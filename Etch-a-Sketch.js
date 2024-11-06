66f8e3152645caa02e63bbf8ae949ee50b9a1be3
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-06 11:21:39 +0000

    added ability to colour boxes with mouseover

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,11 +1,21 @@
 for (let i = 1; i <= 16; i++){
     for (let i = 1; i <= 16; i++){
         const div = document.createElement("div");
-        div.className = `box ${i}`
+        div.className = `box`
         document.querySelector(".boxCont").appendChild(div);
     }
 
     let div = document.createElement("div");
-        div.className = "box17"
+        div.className = "break"
         document.querySelector(".boxCont").appendChild(div);
+}
+
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach(createListeners);
+
+function createListeners (item){
+   item.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "black";
+   }) 
 }
