445489619dab509ad3595284cb526ce9e58ae0f2
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-06 22:07:32 +0000

    resized and centred grid

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -24,23 +24,22 @@ function createGrid (param){
         
 }
 
-
 document.querySelector(".newGrid").addEventListener("click", () => {
     let boxNum = prompt("How wide do you want your grid?")
 
-    document.querySelectorAll(".row").forEach((item) => {
-        item.remove();
-    })
-    document.querySelectorAll(".column").forEach((item) => {
-        item.remove();
-    })
 
     if(boxNum <= 100){
-    createGrid(boxNum);} else if(boxNum === null) {
-        createGrid(16)} else if(boxNum > 100){
-            alert("Grid too large! Pick a number between 1-100!")
-            } else {
-                alert("Pick a number between 1-100!")}
+        document.querySelectorAll(".row").forEach((item) => {
+            item.remove();
+        })
+        document.querySelectorAll(".column").forEach((item) => {
+            item.remove();
+        })    
+        createGrid(boxNum);} else if(boxNum === null) {
+            createGrid(16)} else if(boxNum > 100){
+                alert("Grid too large! Pick a number between 1-100!")
+                } else {
+                    alert("Pick a number between 1-100!")}
 
     
 })
