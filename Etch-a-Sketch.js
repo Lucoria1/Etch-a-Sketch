fe173e8804181e5230db3d8c4916540182710b86
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-07 21:50:18 +0000

    added progressive darkening (shading) button and functionality

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -4,6 +4,8 @@ function createGrid (param){
     let colNum = param;
     let rowNum = param;
 
+    let rowCount = 1;
+
     for (let i = 1; i <= colNum; i++){
         const colDiv = document.createElement("div");
         colDiv.className = `column num${i}`;
@@ -11,54 +13,61 @@ function createGrid (param){
         
             for (let j = 1; j <= rowNum; j++){
                 const rowDiv = document.createElement("div");
-                rowDiv.className = `row ${i}`;
+                rowDiv.className = `row ${rowCount}`;
                 document.querySelector(`.num${i}`).appendChild(rowDiv);
+                rowCount++
             }
         }
-    
-        let mouseCount = 0;
-
-        document.querySelectorAll(".row").forEach((item) => {
-            item.addEventListener("mouseover", (event) => {
-                event.target.style.backgroundColor = "black";
-                mouseCount++;
-                // console.log(mouseCount);
-                })
-        })
-
-        document.querySelector(".Rainbow").addEventListener("click", () => {
-            document.querySelectorAll(".row").forEach((item) => {
-                item.addEventListener("mouseover", (event) => {
-        
-                    let r = (Math.random() * 255).toFixed(0);
-                    // console.log (r);
-        
-                    let g = (Math.random() * 255).toFixed(0);
-                    // console.log (g);
-        
-                    let b = (Math.random() * 255).toFixed(0);
-                    // console.log (b);
-                    
-                    event.target.style.backgroundColor = `rgb(${r},${g},${b})`;
-                    mouseCount++;
-                    // console.log(mouseCount);
-                    })
-            });
-        
-        })
-
-        document.querySelector(".Black").addEventListener("click", () => {
-            document.querySelectorAll(".row").forEach((item) => {
-                item.addEventListener("mouseover", (event) => {
-                    event.target.style.backgroundColor = "black";
-                    mouseCount++;
-                    // console.log(mouseCount);
-                    })
-            });
-        
-        })
-        
 }
+    
+
+document.querySelectorAll(".row").forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+        event.target.style.opacity = "100%";
+        })
+})
+
+
+document.querySelector(".Rainbow").addEventListener("click", () => {
+    document.querySelectorAll(".row").forEach((item) => {
+        item.addEventListener("mouseover", (event) => {
+
+            let r = (Math.random() * 255).toFixed(0);
+
+            let g = (Math.random() * 255).toFixed(0);
+
+            let b = (Math.random() * 255).toFixed(0);
+
+            event.target.style.backgroundColor = `rgb(${r},${g},${b}`;
+            event.target.style.opacity = "100%";
+            })
+    });
+
+})
+
+document.querySelector(".Black").addEventListener("click", () => {
+    document.querySelectorAll(".row").forEach((item) => {
+        item.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black";
+            event.target.style.opacity = "100%"
+            })
+    });
+
+})
+
+document.querySelector(".shading").addEventListener("click", () => {
+    document.querySelectorAll(".row").forEach((item) => {
+        let opacity = 0;
+        item.addEventListener("mouseover", (event) => {
+            event.target.style.opacity = `${opacity}%`
+            opacity += 10;
+            })
+    })
+})
+
+        
+
 
 document.querySelector(".newGrid").addEventListener("click", () => {
     let boxNum = prompt("How wide do you want your grid?")
